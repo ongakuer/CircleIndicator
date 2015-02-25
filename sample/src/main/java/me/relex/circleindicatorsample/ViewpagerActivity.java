016312e748e8593514191144367a27c2e4d4fd44
Relex <relex@skyplatanus.com>
2015-02-25 11:57:03 +0800

    merge ci_drawable_unselected

--- sample/src/main/java/me/relex/circleindicatorsample/ViewpagerActivity.java
+++ sample/src/main/java/me/relex/circleindicatorsample/ViewpagerActivity.java
@@ -19,13 +19,6 @@ public class ViewpagerActivity extends ActionBarActivity {
         defaultViewpager.setAdapter(defaultPagerAdapter);
         defaultIndicator.setViewPager(defaultViewpager);
 
-        // UNSELECTED BACKGROUND
-        ViewPager unselectedBackgroundViewPager = (ViewPager) findViewById(R.id.viewpager_unselected_background);
-        CircleIndicator unselectedBackgroundIndicator = (CircleIndicator) findViewById(R.id.indicator_unselected_background);
-        DemoPagerAdapter unselectedBackgroundPagerAdapter = new DemoPagerAdapter(getSupportFragmentManager());
-        unselectedBackgroundViewPager.setAdapter(unselectedBackgroundPagerAdapter);
-        unselectedBackgroundIndicator.setViewPager(unselectedBackgroundViewPager);
-
         // CUSTOM
         ViewPager customViewpager = (ViewPager) findViewById(R.id.viewpager_custom);
         CircleIndicator customIndicator = (CircleIndicator) findViewById(R.id.indicator_custom);
@@ -45,5 +38,15 @@ public class ViewpagerActivity extends ActionBarActivity {
 
             }
         });
+
+        // UNSELECTED BACKGROUND
+        ViewPager unselectedBackgroundViewPager =
+                (ViewPager) findViewById(R.id.viewpager_unselected_background);
+        CircleIndicator unselectedBackgroundIndicator =
+                (CircleIndicator) findViewById(R.id.indicator_unselected_background);
+        DemoPagerAdapter unselectedBackgroundPagerAdapter =
+                new DemoPagerAdapter(getSupportFragmentManager());
+        unselectedBackgroundViewPager.setAdapter(unselectedBackgroundPagerAdapter);
+        unselectedBackgroundIndicator.setViewPager(unselectedBackgroundViewPager);
     }
 }
