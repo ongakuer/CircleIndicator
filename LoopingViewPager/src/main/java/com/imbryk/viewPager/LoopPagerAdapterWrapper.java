f7cc10b33e8c36720bea0ec6b6b0927d8d70bd87
vikasraimeesho <100354478+vikasraimeesho@users.noreply.github.com>
2022-11-24 22:12:23 +0530

    migrate to androidX and targetSDK 31

--- LoopingViewPager/src/main/java/com/imbryk/viewPager/LoopPagerAdapterWrapper.java
+++ LoopingViewPager/src/main/java/com/imbryk/viewPager/LoopPagerAdapterWrapper.java
@@ -16,9 +16,9 @@
 package com.imbryk.viewPager;
 
 import android.os.Parcelable;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.PagerAdapter;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
 import android.util.SparseArray;
 import android.view.View;
 import android.view.ViewGroup;
--- circleindicator/src/main/java/me/relex/circleindicator/SnackbarBehavior.java
+++ circleindicator/src/main/java/me/relex/circleindicator/SnackbarBehavior.java
@@ -1,9 +1,9 @@
 package me.relex.circleindicator;
 
 import android.content.Context;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.ViewCompat;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.core.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.View;
 import java.util.List;
